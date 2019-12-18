210a9526d551119e9de7a36a835d039f1a2b32b9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-12-18 21:52:29 +0000

    create basic structure

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const isDocLine = line => /^\s+-{2}\s+@/.test(line);
+const isDescriptionLine = line => /^\s+-{2}\s+[^@]/.test(line);
+const isParamLine = line => /^\s+-{2}\s+@param/.test(line);
+const isReturnLine = line => /^\s+-{2}\s+@return/.test(line);
+
+const normalizeDescription = value => value.replace(/^\s+-{2}\s+/, '');
+const normalizeReturn = value => value.replace(/\s+{/, '').replace('}', '');
+const normalizeParam = value =>
+  value
+    .replace(/\s+\{/g, ':')
+    .replace('}', '')
+    .replace(/^\s/, '');
+
+fs.readFile('./src/array.lua', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const lines = data.split('\n');
+  const output = [];
+  let hash = {};
+
+  lines.forEach((line, key) => {
+    if (isDocLine(line)) {
+      if (isDescriptionLine(lines[key - 1])) {
+        hash = {};
+        hash.description = normalizeDescription(lines[key - 1]);
+      } else if (isParamLine(line)) {
+        const param = normalizeParam(line.split('@param')[1]);
+        hash.params = hash.params || [];
+        hash.params.push(param);
+      } else if (isReturnLine(line)) {
+        hash.return = normalizeReturn(line.split('@return')[1]);
+        output.push(hash);
+      }
+    }
+  });
+
+  console.log(output);
+});
