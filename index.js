2181473a9a30702ef3be244ca2939f83cdad8aa8
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-12-20 20:54:38 +0000

    improve template layout

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
-const { readFileAsync } = require('./lib/readFileAsync');
 const { parseFile } = require('./lib/parseFile');
 const { generatePage } = require('./lib/generatePage');
 
 (async () => {
-  const fileContent = await readFileAsync(process.argv[2]);
-  const data = parseFile(fileContent);
+  const filename = process.argv[2];
+  const data = await parseFile(filename);
   console.log(await generatePage(data));
 })();
